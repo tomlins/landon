c33771a9f01bb9a1e98f062925ecb1d4b0bb4829
Jason S Tomlins <jason.s.tomlins@tmr.qld.gov.au>
2022-05-12 10:39:10 +1000

    Another simple change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload, oh yeah, oh boy.
+          Edit <code>src/App.js</code> and save to reload, roger dodger.
         </p>
         <a
           className="App-link"
