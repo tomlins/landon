8c5748e858cb1d84aed6f866879e210a58dc71f1
Jason S Tomlins <jason.s.tomlins@tmr.qld.gov.au>
2022-05-17 10:15:34 +1000

    Added tables to store page content

---
+++ src/scripts/CreateAccessibilityTable.js
@@ -0,0 +1,32 @@
+var AWS = require("aws-sdk");
+var proxy = require('proxy-agent');
+
+AWS.config.update({
+  region: "ap-southeast-2"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+  TableName: "AccessibilityFeatures",
+  
+  KeySchema: [
+    // Partition Key
+    { AttributeName: "name", KeyType: "HASH" }
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "name", AttributeType: "S" }
+  ],
+  
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10
+  }
+};
+
+dynamodb.createTable(params, function(err, data) {
+  if (err)
+    console.error("Unable to create table: ", JSON.stringify(err, null, 2))
+  else
+    console.log("Created table with description: ", JSON.stringify(data, null, 2))
+});
